9f619dfa6d3457ffec3dfbece28b140ca000deeb
Sandra Smith <toosandra@gmail.com>
2020-07-11 15:17:21 -0400

    Issues with VS code, not all my git adds and git commits are being saved.  Created the Manager file and its code, similar to the customer -- areas of change is coding for products, inventory and adding to the inventory, increasing the qty of the stock/inventory and adding new products.  Additionally this is where concoleTable was added, this  is an npm package, it displays ot the console.log.  Then created the Supervisor file, coding same up to where view sales is coded -- in this code block, queries for products and departments are combined, mathematics are run on inventory and sales.

--- bamazonSupervisor.js
+++ bamazonSupervisor.js
@@ -24,3 +24,47 @@ connection.connect(function(error){
 	// start the app
 	welcome();
 });
+
+// welcome function asks what supervisor wants to do and runs corresponding function
+function welcome() {
+	inquirer.prompt([
+		{
+			name: "action",
+			type: "list",
+			choices: ["View Product Sales By Department", "Create New Department", "Exit"],
+			message: "Please select what you would like to do."
+        },
+    ]).then(function(response) {
+		if (response.action === "View Product Sales By Department") {
+			viewSales();
+		} else if (response.action === "Create New Department") {
+			createDepartment();
+		} else if (response.action === "Exit") {
+			exit();
+		}
+	});
+}
+
+// function to view departmental sales
+function viewSales() {
+	// building query variable for inner join
+	// selecting dept id, dept name, OH costs, sum(product sales) with alias product
+	// sales, and sum(product sales) - OH costs with alias total profit - these will
+	// be the resulting table headers
+	// from departments table, inner join products table where dept names equal each 
+	// other. group by dept id
+	var joinQuery = "SELECT department_id, deparments.department_name, over_head_costs,"
+		+ " SUM(product_sales) AS product_sales," 
+		+ " SUM(product_sales) - over_head_costs AS total_profit ";
+	joinQuery += "FROM deparments INNER JOIN products ";
+	joinQuery += "ON deparments.department_name = products.department_name ";
+    joinQuery += "GROUP BY department_id ";
+    
+    // query the db, throw error if error, if not, build and print console table
+	// return to welcome screen
+	connection.query(joinQuery, function(error, results) {
+		if (error) throw error;
+		consoleTableProfit("\nDepartmental Profit", results);
+		welcome();
+	});
+}
